a4adb0ee9bf34ea866839e4b93d6b29e316afd38
KILLVader8550 <145247735+KILLVader8550@users.noreply.github.com>
2025-02-11 23:09:36 +0700

    The final version of this app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 async function getData () {
   const response = await fetch('https://catfact.ninja/fact')
-  const data = await response.json()
-  return data
+  const data = await response.json();
+  return data;
 }
 
 function App() {
   const [fact, setFact] = useState('');
+  const hasFetched = useRef(false)
+
+  async function fetchFact() {
+    const catFcat =  await getData();
+    setFact(catFcat.fact);
+  }
   
   useEffect(() => {
-    async function fetchFact() {
-      const catFcat =  await getData();
-      setFact(catFcat.fact);
+    if (!hasFetched.current) {
+      fetchFact();
+      hasFetched.current = true;
     }
-    fetchFact();
   },[]);
   
   return (
     <div className='container'>
       <div className='cat-fact'>{fact}</div>
-      <button id='new'>New</button>
+      <button id='new' onClick={fetchFact}>New</button>
     </div>
   )
 }
